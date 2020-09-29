ecbb7344f6685ab89d028c35ba522703e883208c
Orlando Hutapea <orlando.timotius.hutapea@gmail.com>
2020-09-29 17:58:11 +0700

    Added scrolling animations

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const delayPerPath = .25
+const DELAY_PER_PATH = .25 // in seconds
+const SCROLLING_DELAY = .5 // in seconds
+
+let isSmoothScrolling = false
+let isOnSection = false
 
 function removeLogo(logoFinishedDelay) {
   setTimeout(() => {
@@ -30,24 +34,47 @@ function animateLogo() {
     path.css({
       'stroke-dasharray': `${pathLength}px`,
       'stroke-dashoffset': `${pathLength}px`,
-      'animation': `logo-stroke 2s ease forwards ${index*delayPerPath}s`
+      'animation': `logo-stroke 2s ease forwards ${index*DELAY_PER_PATH}s`
     })
   })
 
-  let logoFinishedDelay = (logoPaths.length + 1)*delayPerPath
+  let logoFinishedDelay = (logoPaths.length + 1)*DELAY_PER_PATH
   $('#logo').css('animation', `logo-fill 0.5s ease forwards ${logoFinishedDelay}s`)
   removeLogo(logoFinishedDelay)
 }
 
 animateLogo()
 
-function setBackground() {
+$(window).on('resize load', () => {
   let background = $('.background')
-  background.css('height', `${document.body.getBoundingClientRect().bottom-$('main section:first-child')[0].getBoundingClientRect().top}px`)
-  $('.background-header').css('height', `${background[0].getBoundingClientRect().top-$('.header-text')[0].getBoundingClientRect().top}px`)
-}
+  background.css('height', `${$(document.body).height()-$('header').height()}px`)
+  $('.background-header').css('height', `${background.offset().top-$('.header-text').offset().top}px`)
 
-$(window).on('resize', setBackground)
-$(window).on('load', setBackground)
+  isOnSection = $(window).scrollTop() >= $('main section:first-child').offset().top
+})
+
+$(window).on('scroll', () => {
+  if (isSmoothScrolling) return
+
+  let viewTop = $(window).scrollTop()
+  let viewBottom = viewTop + $(window).height()
+  let firstSectionTop = $('main section:first-child').offset().top
+
+  if (!isOnSection && viewBottom >= firstSectionTop) {
+    isSmoothScrolling = true
+
+    $('html, body').animate({ scrollTop: firstSectionTop }, SCROLLING_DELAY*1000, () => {
+      isOnSection = true
+      isSmoothScrolling = false
+    })
+  } else if (isOnSection && viewTop < firstSectionTop) {
+    isSmoothScrolling = true
+
+    $('html, body').animate({ scrollTop: 0 }, SCROLLING_DELAY*1000, () => {
+      isOnSection = false
+      isSmoothScrolling = false
+    })
+  }
+})
 
 $('#username-ipa').on('click', () => $('#username-ipa-audio')[0].play())
